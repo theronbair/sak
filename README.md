25ed698d96ecdffebdaec1e43f29a5585ce750ec
Theron Bair <theron.bair@themonger.com>
2022-01-18 11:57:36 -0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sak
+My personal Swiss Army Knife toolbox for Go stuff.
+
+I use it.  You may.  Or may not.  It's chock-full of my own solecisms, subject to evolution as needed.
---
+++ sak.go
@@ -0,0 +1,66 @@
+package sak
+
+import (
+   "fmt"
+   "time"
+   "strconv"
+   spew "github.com/davecgh/go-spew/spew"
+)
+
+const version = "1.0.0"
+
+type L struct {
+   f string       // "facility" equivalent
+   s string       // "severity" equivalent
+   c string       // error code/key string
+}
+
+type Options struct {
+   DebugLevel int
+   Behavior struct {
+      PrintTime bool
+      TimeMilli bool
+   }
+}
+
+var (
+   opts = Options{}
+   now = time.Now()
+   nowNano = now.UnixNano()
+   nowMilli = nowNano / 1000000
+   nowMilli_str = strconv.FormatInt(nowMilli, 10)
+   now_str = strconv.FormatInt(nowMilli / 1000, 10)
+)
+
+// can be used for program output; specify n = 0 and no facility
+
+func LOG(n int, logopts L, msgs ...interface{}) {
+   if ( opts.DebugLevel >= n ) {
+      if ( n > 0 ) {
+         fmt.Printf("%d:", n)
+      }
+      if ( opts.Behavior.PrintTime ) {
+         if ( opts.Behavior.TimeMilli ) {
+            fmt.Printf("%s:", nowMilli_str)
+         } else {
+            fmt.Printf("%s", now_str)
+         }
+      }
+      if ( logopts.f != "" ) {
+         fmt.Printf("%s:", logopts.f)
+      }
+      if ( n > 0 || logopts.f != "" ) {
+         fmt.Printf(" ")
+      }
+      for m := range msgs {
+         switch msgs[m].(type) {
+            case string:
+               fmt.Printf("%s", msgs[m].(string))
+            case error:
+               fmt.Printf("%s", msgs[m].(error).Error())
+            default:
+               fmt.Printf("%s", spew.Sdump(msgs[m]))
+         }
+      }
+   }
+}
